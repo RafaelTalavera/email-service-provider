e1b5724e2ad4e5f386743f2360ab6be13d7e5dfb
RafaelTalavera <rtalavera@live.com.ar>
2023-11-16 08:17:47 -0300

    initial commit

---
+++ src/test/java/com/axioma/emailserviceprovider/EmailServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axioma.emailserviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
